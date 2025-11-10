a1d51a817d07bec557f036284d6a2596e0b47024
Tselote Woldemichael <tselotewoldemicheal@gmail.com>
2025-11-09 20:59:39 -0500

    add proof of concept shapes

--- main.py
+++ main.py
@@ -1,62 +1,80 @@
-from pydrake.all import RigidTransform, RollPitchYaw, StartMeshcat
-from manipulation.letter_generation import create_sdf_asset_from_letter
-import time
-
-
-import os
-from pathlib import Path
-
-import matplotlib.pyplot as plt
-import numpy as np
-import trimesh
 from pydrake.all import (
-    AddFrameTriadIllustration,
-    BasicVector,
-    Concatenate,
-    Context,
-    Diagram,
     DiagramBuilder,
-    Integrator,
-    JacobianWrtVariable,
-    LeafSystem,
-    MultibodyPlant,
-    PiecewisePolynomial,
-    PiecewisePose,
-    PointCloud,
-    Rgba,
-    RigidTransform,
-    RobotDiagram,
-    RollPitchYaw,
-    RotationMatrix,
     Simulator,
     StartMeshcat,
-    Trajectory,
-    TrajectorySource,
 )
 
-from manipulation import running_as_notebook
-from manipulation.exercises.grader import Grader
-from manipulation.icp import IterativeClosestPoint
-from manipulation.letter_generation import create_sdf_asset_from_letter
-from manipulation.meshcat_utils import AddMeshcatTriad
 from manipulation.station import (
-    AddPointClouds,
     LoadScenario,
     MakeHardwareStation,
-    RobotDiagram,
 )
-from manipulation.utils import RenderDiagram
+from pathlib import Path
 
 
+# Start meshcat for visualization
+meshcat = StartMeshcat()
+print("Click the link above to open Meshcat in your browser!")
 
-# initial pose / target pose / grasp pose functions
+# pieces to choose from
+triangle_sdf_path = Path("/Users/jity/Desktop/6.4210/PuzzleBot/assets/green_triangle.sdf")
+triangle_sdf_uri = triangle_sdf_path.resolve().as_uri()
 
-# motion planning function
+square_sdf = ""
+cross_sdf = ""
+semicircle_sdf = ""
+hexagon_sdf = ""
 
-# controller class
+# puzzle pieces
+upper_left_corner_sdf = ""
+lower_left_corner_sdf = ""
+upper_right_corner_sdf = ""
+lower_right_corner_sdf = ""
 
-# inverse kinematics function (spatial position -> q)
 
-# main function (that builds diagram + connects everything together)
+scenario_string = f"""directives:
+- add_model:
+    name: iiwa
+    file: package://drake_models/iiwa_description/urdf/iiwa14_primitive_collision.urdf
+    default_joint_positions:
+      iiwa_joint_1: [-1.57]
+      iiwa_joint_2: [0.1]
+      iiwa_joint_3: [0]
+      iiwa_joint_4: [-1.2]
+      iiwa_joint_5: [0]
+      iiwa_joint_6: [1.6]
+      iiwa_joint_7: [0]
+- add_weld:
+    parent: world
+    child: iiwa::iiwa_link_0
 
-# table sdf / five puzzle sdfs / almost complete puzzle sdf
+- add_model:
+    name: wsg
+    file: package://manipulation/hydro/schunk_wsg_50_with_tip.sdf
+- add_weld:
+    parent: iiwa::iiwa_link_7
+    child: wsg::body
+    X_PC:
+        translation: [0, 0, 0.09]
+        rotation: !Rpy {{ deg: [90, 0, 90]}}
+
+- add_model:
+    name: table
+    file: package://manipulation/table.sdf
+- add_weld:
+    parent: world
+    child: table::table_link
+    X_PC:
+        translation: [0.0, 0.0, -0.05]
+        rotation: !Rpy {{ deg: [0, 0, -90] }}
+- add_model:
+    name: green_triangle_piece
+    file: "{triangle_sdf_uri}"
+"""
+scenario = LoadScenario(data=scenario_string)
+station = MakeHardwareStation(scenario, meshcat=meshcat)
+builder = DiagramBuilder()
+builder.AddSystem(station)
+diagram = builder.Build()
+simulator = Simulator(diagram)
+simulator.set_target_realtime_rate(1.0)
+simulator.AdvanceTo(50)
