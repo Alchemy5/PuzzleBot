e5876dad7bbe7222c281290097728560f798174e
Varun Hariprasad <varunhpr5@gmail.com>
2025-11-17 22:50:31 -0500

    working with positive space detection for new point cloud data but fails for negative space

--- main.py
+++ main.py
@@ -342,17 +342,55 @@ puzzle_cloud, tray_clouds = get_puzzle_and_tray_pointclouds(
 )
 
 ####### TODO: Move following section to its own separate file ########
-points = puzzle_cloud.xyzs().T
+puzzle_points = puzzle_cloud.xyzs().T
+for piece in tray_clouds:
+    cloud = tray_clouds[piece]
+    points = cloud.xyzs().T
+    center1, center2 = find_z_centers(puzzle_points)
+
+    min_center = min(center1, center2)
+    max_center = max(center1, center2)
+
+    # we want max center now
+    positive_space_points = []
+    for point in points:
+        closest_center = find_closest_z_center(point, min_center, max_center)
+        if closest_center == max_center:
+            positive_space_points.append(point)
+
+    largest_positive_region = largest_region(positive_space_points)
+
+    pos = np.asarray(largest_positive_region)
+    cloud_pos = PointCloud(new_size=pos.shape[0])
+    cloud_pos.mutable_xyzs()[:] = pos.T
+    meshcat.SetObject(
+        piece,
+        cloud_pos,
+        point_size=0.01,
+        rgba=Rgba(0.0, 1.0, 0.0),
+    )
+
+
 # Step 1: Identify negative space
-center1, center2 = find_z_centers(points)
+center1, center2 = find_z_centers(puzzle_points)
 min_center = min(center1, center2)  # corresponds to negative space
 max_center = max(center1, center2)  # corresponds to boundary puzzle pieces
 
 negative_space_points = []
-for point in points:
+for point in puzzle_points:
     closest_center = find_closest_z_center(point, min_center, max_center)
     if closest_center == min_center:
         negative_space_points.append(point)
+
+neg_pts = np.asarray(puzzle_points)
+cloud_neg = PointCloud(new_size=neg_pts.shape[0])
+cloud_neg.mutable_xyzs()[:] = neg_pts.T
+meshcat.SetObject(
+    "negative_space",
+    cloud_neg,
+    point_size=0.01,
+    rgba=Rgba(0.0, 1.0, 0.0),
+)
 
 # now choose largest continuous region for these negative space points
 
